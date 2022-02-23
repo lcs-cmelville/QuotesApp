b1aafa365ba02cff70715e29d4dc8fb08b77b74f
Claire Melville <cmelville@lcs.on.ca>
2022-02-23 09:46:31 -0500

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Claire Coding Account on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
